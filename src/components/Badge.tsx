52918b4a82b62837c0ac7cc15176f78e153acf2a
smartmindvr <info@digitalcombines.it>
2024-01-14 17:08:40 +0100

    Watson Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      <b>Doctor Watson</b> Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.smartmindnet.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Smart Mind</span>
       </a>
     </span>
   );
